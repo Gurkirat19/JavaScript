d4095d7b46916c5b76d7c817b827ed64b2526006
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-02 07:13:36 +0000

    javascript

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 145656
+let accountEmail = "gurkirat@google.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountstate;
+
+// accountId = 2
+
+accountEmail = "nice@gmail.com"
+accountPassword = "6789"
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // it treat all js code as newer version
+
+// alert(3 + 3) it cannot be directly use in nodejs but can be use in the browser directly
+
+let name = "gurkirat"
+let age = 18
+let isLoggedIn = false
+
+// number = 2 to power 53
+// biginit -> if number is very big then we use this
+// string -> written in quotes
+// boolena -> true/false
+// null -> standalone value (empty value)
+// undefined -> no value assigned 
+// symbol -> use to defined / unique values
+// object 
+
+console.log(typeof "gurkirat"); 
+// typeof used to define the type of the data 
+// here the null -> object
+// and for the undefined -> undefined
+
