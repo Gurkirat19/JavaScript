20055904374a51dede8f913d6c8aded9f0bb6443
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-02 07:42:50 +0000

    conversion_operations

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(valueInNumber);
+
+// if value is null then -> 0
+// if value is undefined -> NaN
+// if vaue is boolean (true/false) - (1/0)
+// if value is string -> 
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// if value is "" -> false
+// if value is "gurkirat" -> true
+
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
