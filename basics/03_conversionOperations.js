c4d4d9d495c62440cddf29c32294d09f03590e4f
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-03 06:17:31 +0000

    conversionOperation

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+// ******************************Convertions*************************
+
 let score = "33abc"
 
 // console.log(typeof score);
@@ -20,7 +22,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // if value is "" -> false
 // if value is "gurkirat" -> true
 
@@ -28,5 +30,51 @@ console.log(booleanIsLoggedIn);
 let somenumber = 33
 
 let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+
+
+// ******************operations**************************>
+
+let value = 3
+let negvalue =-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " gurkirat"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+// prefix - here the first value is changed first then the original value is changed 
+// postfix - here the the first value is assigned to the second variable then the value is changed
+
