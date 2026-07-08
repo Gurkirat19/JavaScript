7527e8833892c6b288d60c90937f69bbfcff08cd
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-08 12:24:50 +0000

    date&time

---
+++ basics/10_date&Time.js
@@ -0,0 +1,46 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// here we have converted the date to the string so it can be rea easily 
+
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// we can convert them into multiple things 
+
+
+// console.log(typeof myDate);
+// date is object
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+
+
+
+// -------------------------------------X-------------------------------
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time is ${}`
+
+newDate.toLocaleString('default', {
+    weekday : "long",
+})
+
+
