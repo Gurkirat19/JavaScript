6c6059631e2f90a97834f9f02ac30794cf169b93
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-13 12:59:32 +0000

    array

---
+++ 11_arrays.js
@@ -0,0 +1,6 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+// array can contain any value including strings, number, characters.
+const myHeroes = ["shaktiman", "ironman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0]);
