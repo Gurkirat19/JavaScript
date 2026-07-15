2b29fe70edc42f4311e308459d49ada0f2252729
GURKIRAT SINGH <149244395+Gurkirat19@users.noreply.github.com>
2026-07-15 12:28:58 +0000

    arrays1,2

--- 11_arrays.js
+++ 11_arrays.js
@@ -4,3 +4,46 @@ const myHeroes = ["shaktiman", "ironman"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 console.log(myArr[0]);
+
+
+
+// push - used in array to add new items 
+myArr.push(6)
+myArr.push(7)
+console.log(myArr);
+
+// pop - used to remove the items (removes the last item)
+myArr.pop()
+console.log(myArr);
+
+// unshift - used to add items (add item in the front of arr)
+myArr.unshift(8);
+console.log(myArr);
+
+// shift - used to remove the items (remove from the starting)
+myArr.shift();
+console.log(myArr);
+
+// includes - check whether the arr has item or not 
+console.log(myArr.includes(4));
+
+// indexof - checks the desired value is present or not 
+console.log(myArr.indexOf(19));
+
+// .join - it creates and return new string in the arr
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+
+// .slice - extraxts the portion of an arr or string
+//         (means using the rnage we can remove the part of it)
+console.log("a", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+// .splice - it manipulates the array 
+//           (also extracts the postion of an array and gives the items)
+const myn2 = myArr.splice(1,3);
+console.log(myn2); 
+
+
---
+++ 11_arrays2.js
@@ -0,0 +1,41 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes);
+// here it will merge the arrays but it will convert the
+// second array into the 4th element in the first arr although it is a array.
+// here push changes the original array 
+
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+console.log(allHeroes);
+// .concat - Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+
+
+const allHeroes1 = [...marvel_heroes, ...dc_heroes]
+console.log(allHeroes1);
+// this is called spread out values 
+// here we use ... so that they give me imvidual values
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_array = another_array.flat(Infinity)
+console.log(real_array);
+// .flat - if we have to concat the multiple arrays merged in one so we use flat function
+//         to separate them singly and tell how much depth is there or use infinity instead of it  
+
+
+console.log(Array.isArray("Gurkirat"));
+// .isArray - this is used to check whether it is array or not 
+
+
+console.log(Array.from("Gurkirat"))
+// .from - this is used to convert any type of datatypes into the array
+
+
+console.log(Array.from({name: "gurkirat"}));
+//  this will give the empty array so we have to define which values should be convert into arry keys or values
+
+
+console.log(Array.of(score1, score2, score3));
+// .of - Returns a new array from a set of elements.
